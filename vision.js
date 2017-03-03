555a26e34981ad093b86bc7b2e1fd69b08e1c470
Clifford <chocm7@gmail.com>
2017-03-02 23:13:32 -0800

    fin1
    fin1

--- vision.js
+++ vision.js
@@ -8,7 +8,7 @@ function bindButtons(){
     var payload = {url:null};
     payload.url = document.getElementById("urlCode").value;
     //req.open("POST", "https://westus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Description,Tags,Categories,Faces,ImageType,Color,Adult&details=Celebrities&language=en", true);
-    req.open("POST", "https://westus.api.cognitive.microsoft.com/vision/v1.0/describe?maxCandidates=1", true);
+    req.open("POST", "https://westus.api.cognitive.microsoft.com/vision/v1.0/tag", true);
     req.setRequestHeader('Content-Type', 'application/json');
     req.setRequestHeader("OCP-Apim-Subscription-Key", "f94adc036b6c4e57b65ffafa8c8cf3ff");
     req.addEventListener('load', function() {
